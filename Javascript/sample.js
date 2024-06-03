9534cd39423cddc2e8297fdca863e80a9ad74373
Manibharathi <manibharath159@gmail.com>
2024-06-03 19:25:45 +0530

    Javascript Variable

---
+++ Javascript/sample.js
@@ -0,0 +1,21 @@
+//Variable Creation
+var myName; // Declaration
+myName = "Manibharathi"; // Value Initialization
+var age = 35;
+var weight = 65.5;
+var isFound = true;
+/*console.log(myName);
+console.log(age);
+console.log(weight);
+console.log(isFound);
+myName = "Surya";
+console.log(myName);*/
+
+//var salary = "56000";
+
+var firstNumber = "2";
+var secondNumber = "5";
+var result = firstNumber + secondNumber;
+console.log(result);
+
+
